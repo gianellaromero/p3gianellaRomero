3f623f0c775a903c32adafd88da126e12b010153
gianellaromero <romerogianellaa@gmail.com>
2023-04-13 19:21:25 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('welcomee', function () {
-    return view('index');
-});
+
+Route::resource('students',StudentController::class);
